69ce6c66f4bd5bed08a27308a18e153f76effd1d
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-08-29 20:45:12 +0300

    a

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -56,21 +56,21 @@ class PostCreateFormTests(TestCase):
             Post.objects.filter(
                 text='Тестовый текст',
                 group='Тестовая группа'
-                    ).exists()
-                )
+                        ).exists()
+                    )
 
     def post_edit_(self):
         post = self.cls.post
         form_data_edit = {
             'group': 'Тестовая группа',
             'text': 'Тестовый отредоктированный текст',
-                }
+                    }
 
         response = self.author_client.post(
             reverse('posts:post_edit'),
             data=form_data_edit,
             follow=True
-                )
+                    )
         self.assertRedirects(response, reverse(
             'profile', kwargs={'username': 'test_user'}))
         self.assertNotEqual(post, response)
