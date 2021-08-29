abfc468cda478967cf2cc4dff6ba66296f87dbb1
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-08-29 22:04:22 +0300

    Update test_views.py

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -49,5 +49,6 @@ class PostPagesTests(TestCase):
         """Шаблон post_list сформирован с правильным контекстом."""
         response = (self.authorized_client.get(
             reverse('posts:group_posts', kwargs={'slug': 'test-slug'})))
-        self.assertEqual(response.context.get('cls.post').author, f'{self.user}')
+        self.assertEqual(response.context.get(
+            'cls.post').author, f'{self.user}')
         self.assertEqual(response.context.get('post').text, 'тестовый текст')
